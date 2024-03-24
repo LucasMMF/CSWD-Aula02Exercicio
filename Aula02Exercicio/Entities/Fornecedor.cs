64c3273825ed7518e2aec9ea59afdd698efbe1e1
LucasMMF <mateusmidao@gmail.com>
2024-03-24 18:19:05 -0300

    feat: initial program structure and entities

---
+++ Aula02Exercicio/Entities/Fornecedor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02Exercicio.Entities
+{
+    public class Fornecedor
+    {
+        #region Properties
+
+        public Guid IdFornecedor { get; set; }
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+
+        #endregion
+
+        #region Associations
+
+        public List<Produto> Produtos { get; set; }
+
+        #endregion
+    }
+}
---
+++ Aula02Exercicio/Entities/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula02Exercicio.Entities
+{
+    public class Produto
+    {
+        #region Properties
+
+        public Guid IdProduto { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+
+        #endregion
+
+        #region Associations
+
+        public Fornecedor Fornecedor { get; set; }
+
+        #endregion
+    }
+}
